693f460dd77fcb1729a6a7c474960ca0cf809df1
Cuong Liam <cuongdeptrai2706@gmail.com>
2025-06-09 17:50:28 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr.length - i - 1; j ++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,23,5,2,1,4,66,22,12,9,1,2,4,9};
+
+        bubbleSort(arr);
+
+        for (int i : arr){
+            System.out.println(i);
+        }
+
+    }
+}
